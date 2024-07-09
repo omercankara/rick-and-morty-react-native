11b62e464533f8b29fabcfbbb427ee57a969bc6f
Ömercan <omercankara3434@gmail.com>
2024-07-10 02:06:40 +0300

    rick-and-morty-react-native
    rick-and-morty-react-native

--- Screen/Home.js
+++ Screen/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import MoviePagination from "../Components/MoviePagination"; // doğru yolu belirttiğinizden emin olun
+import MoviePagination from "../Components/MoviePagination";
 
 
 const Home = ({ navigation }) => {
